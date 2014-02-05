de675d429763c2031a9e2313a0d945a5ed2293e5
omardelarosa <thedelarosa@gmail.com>
2014-02-04 23:04:55 -0500

    created a linear, series of blinks in the matrix_blink sketch

--- matrix_blink/matrix_blink.ino
+++ matrix_blink/matrix_blink.ino
@@ -1,19 +1,52 @@
-const int pins[4] = {
+const int analogInPin = A0;
+
+const int pins_a[4] = {
   4,5,3,2 };
+  
+const int pins_b[4] = {
+  6,7,8,9 };
+  
+int sensorValue = 0;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600); 
   for (int thisPin = 0; thisPin < 4; thisPin++) {
-    pinMode(pins[thisPin], OUTPUT);
+    pinMode(pins_a[thisPin], OUTPUT);
+  }
+   for (int thisPin = 0; thisPin < 4; thisPin++) {
+    pinMode(pins_b[thisPin], OUTPUT);
   }
 }
 
+int checkSensor(){
+ sensorValue = analogRead(analogInPin);
+// Serial.print("sensor = " );                       
+// Serial.print(sensorValue); 
+ return sensorValue;
+}
+
+int getDelay(){
+// 250 ms = 1 eighth note at 120 bpm
+  return checkSensor()/4;
+}
+
+
 void loop(){
+ 
+ 
  for (int thisPin = 0; thisPin < 4; thisPin++) {
-    digitalWrite(pins[thisPin], HIGH);
-    delay(200);
-    digitalWrite(pins[thisPin], LOW);
-    delay(200); 
+    digitalWrite(pins_a[thisPin], HIGH);
+    delay(getDelay());
+    digitalWrite(pins_a[thisPin], LOW);
+    delay(getDelay()); 
+//    checkSensor();
+  }
+  for (int thisPin = 0; thisPin < 4; thisPin++) {
+    digitalWrite(pins_b[thisPin], HIGH);
+    delay(getDelay());
+    digitalWrite(pins_b[thisPin], LOW);
+    delay(getDelay()); 
+//    checkSensor();
   }
 }
